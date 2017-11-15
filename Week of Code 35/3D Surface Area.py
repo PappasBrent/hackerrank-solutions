7ce0e820f79e008d421d9076fe2dc45901f820ca
Brent Pappas <pappasbrent@gmail.com>
2017-11-15 09:14:44 -0500

    Progress

--- Week of Code 35/3D Surface Area.py
+++ Week of Code 35/3D Surface Area.py
@@ -22,19 +22,24 @@ for i in range(w-1):
         # To the right
         if (board[i][j] < board[i][j+1]):
             total += board[i][j + 1] - board[i][j]
+        # To the left
+
         # Below
         if (board[i][j] < board[i + 1][j]):
             total += board[i + 1][j] - board[i][j]
+        # Above
+        
 
 # Add the sides of the board
+
 for i in range(w):
     # Top / Bottom
     total += board[0][i]
-    total += board[w-1][i]
+    total += board[-1][i]
 
     # Left / Right
     total += board[i][0]
-    total += board[w-1][0]
+    total += board[i][-1]
         
         
 
